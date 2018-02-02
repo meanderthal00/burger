6d4337789d5ec4d88e2c5bb1e276633f9468642e
Meanderthal00 <meanderthal00@yahoo.com>
2018-02-02 17:24:50 -0500

    server underway

--- server.js
+++ server.js
@@ -2,3 +2,11 @@
 var express = require("express");
 var method_override = require("method_override");
 var body_parser = require("body_parser");
+
+// server set up
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+    console.log("listening on port", port);
+  });
+  
