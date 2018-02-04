6d7b4fb309cd4b20b17945e2dd18e56228ece907
Meanderthal00 <meanderthal00@yahoo.com>
2018-02-04 14:36:51 -0500

    main server file being de-bugged

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 // requiring dependencies
 var express = require("express");
-var method_override = require("method_override");
-var body_parser = require("body_parser");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
 
 // server set up
 var port = process.env.PORT || 3000;
 
-app.listen(port, function() {
-    console.log("listening on port", port);
-  });
-  
+var app = express();
+
+//pulling static content for the app from the public folder
+app.use(express.static(".burger/public"));
+
+//parsing the application
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//parsing the JSON object
+app.use(bodyParser.json());
+
+//pulling in handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//importing routes and giving server access
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// setting listener
+app.listen(port, function () {
+  console.log("listening on port", port);
+});
