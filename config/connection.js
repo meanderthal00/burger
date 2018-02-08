5e728edbc16e8b88b78699c9e3db8854cf87670f
Meanderthal00 <meanderthal00@yahoo.com>
2018-02-08 06:25:36 -0500

    code clean up

--- config/connection.js
+++ config/connection.js
@@ -20,18 +20,4 @@ if (process.env.JAWSDB_URL) {
 connection.connect();
 module.exports = connection;
 
-// creating the connection
 
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3000, 
-//     user:"root",
-//     password:"toorresu",
-//     database:"burgers_db"
-// });
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     console.log("connected as id " + connection.threadId);
-//     // call functions here
-// })
-// module.exports = connection;
